50616185354ef9ccc32082a88adc8c21376b98d1
Leonan Costa <leonanfecosta@gmail.com>
2022-03-04 05:12:51 -0300

    resolve o exercicio 4 do dia 7.1

--- fundamentos/bloco-07-introdução-à-javascript-es6-e-testes-unitários/dia-01-let-const-arrow-functions-e-template-literal/exercises2.js
+++ fundamentos/bloco-07-introdução-à-javascript-es6-e-testes-unitários/dia-01-let-const-arrow-functions-e-template-literal/exercises2.js
@@ -17,21 +17,53 @@ console.log(factorial(5)); */
 const factorial = (number) => (number > 1 ? number * factorial(number - 1) : 1);
 console.log(factorial(5));
 
-
 //2. Crie uma função que receba uma frase e retorne qual a maior palavra.
-const maiorPalavra = text => {
-    let wordArray = text.split(' ');
-    let maxLenght = 0;
-    let result = '';
+const maiorPalavra = (text) => {
+  let wordArray = text.split(" ");
+  let maxLenght = 0;
+  let result = "";
 
-    for (const word of wordArray){
-        if (word.length > maxLenght){
-            maxLenght = word.length;
-            result = word
-        }
+  for (const word of wordArray) {
+    if (word.length > maxLenght) {
+      maxLenght = word.length;
+      result = word;
     }
-    return result;
-}
+  }
+  return result;
+};
 
-console.log(maiorPalavra("Antonio foi no banheiro e não sabemos o que aconteceu"));
+console.log(
+  maiorPalavra("Antonio foi no banheiro e não sabemos o que aconteceu")
+);
 
+// Função 1 : Escreva uma função que vai receber uma string como parâmetro. Sua função deverá procurar pela letra x em uma string qualquer que você determinar e substituir pela string que você passou como parâmetro. Sua função deve retornar essa nova string .
+// Exemplo:
+// String determinada: "Tryber x aqui!"
+// Parâmetro: "Bebeto"
+// Retorno: "Tryber Bebeto aqui!"
+
+const stringDeterminada = "Tryber x aqui!";
+const substituirX = (string) => stringDeterminada.replace("x", string);
+
+// Um array com escopo global, que é o escopo do arquivo JS , nesse caso, contendo cinco strings com suas principais skills .
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"];
+
+// Função 2 : Escreva uma função que vai receber a string retornada da Função 1 como parâmetro. Essa função deve concatenar as skills do array global à string que foi passada para a Função 2 via parâmetro. Você deve ordenar os skills em ordem alfabética. Sua função deve retornar essa nova string .
+// Exemplo: "Tryber x aqui! Minhas cinco principais habilidades são:
+// JavaScript;
+// HTML; ... #goTrybe".
+
+array.sort((a, b) => {
+  let x = a.toUpperCase();
+  let y = b.toUpperCase();
+
+  return x == y ? 0 : x > y ? 1 : -1;
+});
+
+const concatenar = (substituirX) => {
+  let result = `${substituirX} Minhas cinco principais habilidades são:
+${array} #goTrybe`;
+  return result;
+};
+
+console.log(concatenar(substituirX("Leonan")));
